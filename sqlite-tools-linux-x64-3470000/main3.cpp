fc51b34f664806adea2d395c37ea213405f59615
Lucas Araujo <lucasaraujo.oliveira@estudante.ufjf.br>
2025-02-21 09:45:23 -0300

    cria as funções "createTable" na main3 e insertData

--- sqlite-tools-linux-x64-3470000/main3.cpp
+++ sqlite-tools-linux-x64-3470000/main3.cpp
@@ -4,6 +4,133 @@
 
 using namespace std;
 
+static int createSimulation(const char* s){
+    sqlite3* DB;
+    string sql = "CREATE TABLE IF NOT EXISTS SIMULATION("
+    "SIMULATION_ID INTEGER PRIMARY KEY, "
+    "OUTPUT_ID INTEGER FOREIGN KEY NOT NULL, "
+    "TIME_DAYS INTEGER NOT NULL, "
+    "SIMULATION_STATUS TEXT NOT NULL);";
+
+    try{
+        int exit = 0;
+        exit = sqlite3_open(s, &DB);
+        char* messageError;
+        exit = sqlite3_exec(DB, sql.c_str(), nullptr, 0, &messageError);
+
+        if(exit != SQLITE_OK){
+            cerr << "Error create table" << endl;
+            sqlite3_free(messageError);
+        }else{
+            cout<<"Table created sucessfully" << endl;
+        }
+        sqlite3_close(DB);
+    }catch(const exception & e){
+        cerr<<e.what();
+    }
+    return 0;
+}
+
+static int createOutput(const char* s){
+    sqlite3* DB;
+    string sql = "CREATE TABLE IF NOT EXISTS OUTPUT("
+    "OUTPUD_ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+    "MODEL_ID INTEGER FOREIGN KEY NOT NULL, "
+    "INPUT_ID NOT NULL, "
+    "RESULTS TEXT NOT NULL);";
+
+    try{
+        int exit = 0;
+        exit = sqlite3_open(s, &DB);
+        char* messageError;
+        exit = sqlite3_exec(DB, sql.c_str(), nullptr, 0, &messageError);
+
+        if(exit != SQLITE_OK){
+            cerr << "Error create table" << endl;
+            sqlite3_free(messageError);
+        }else{
+            cout<<"Table created sucessfully" << endl;
+        }
+        sqlite3_close(DB);
+    }catch(const exception & e){
+        cerr<<e.what();
+    }
+    return 0;
+}
+
+static int createModel(const char* s){
+    sqlite3* DB;
+    string sql = "CREATE TABLE IF NOT EXISTS MODEL("
+    "MODEL_ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+    "MODEL_DESCRIPTION TEXT NOT NULL, "
+    "MODEL_VERSION INTEGER NOT NULL);";
+
+    try{
+        int exit = 0;
+        exit = sqlite3_open(s, &DB);
+        char* messageError;
+        exit = sqlite3_exec(DB, sql.c_str(), nullptr, 0, &messageError);
+
+        if(exit != SQLITE_OK){
+            cerr << "Error create table" << endl;
+            sqlite3_free(messageError);
+        }else{
+            cout<<"Table created sucessfully" << endl;
+        }
+        sqlite3_close(DB);
+    }catch(const exception & e){
+        cerr<<e.what();
+    }
+    return 0;
+}
+
+static int createInput(const char* s){
+    sqlite3* DB;
+    string sql = "CREATE TABLE IF NOT EXISTS INPUT("
+    "INPUT_ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+    "CORTISOL_EXP_PARAMETERS TEXT NOT NULL, "
+    "SIMULATION_GENDER CHAR(1) NOT NULL, "
+    "INITIAL_CONDITIONS TEXT NOT NULL);";
+
+    try{
+        int exit = 0;
+        exit = sqlite3_open(s, &DB);
+        char* messageError;
+        exit = sqlite3_exec(DB, sql.c_str(), nullptr, 0, &messageError);
+
+        if(exit != SQLITE_OK){
+            cerr << "Error create table" << endl;
+            sqlite3_free(messageError);
+        }else{
+            cout<<"Table created sucessfully" << endl;
+        }
+        sqlite3_close(DB);
+    }catch(const exception & e){
+        cerr<<e.what();
+    }
+    return 0;
+}
+
+static int insertData(const char* s){
+    sqlite3* DB;
+    char* messageError;
+
+    int exit = sqlite3_open(s, &DB);
+
+    string sql("INSERT INTO GRADES (NAME, LNAME, AGE, ADDRESS, GRADE) VALUES('Alice', 'Chapa', 35, 'Tampa', 'A');"
+    "INSERT INTO GRADES (NAME, LNAME, AGE, ADDRESS, GRADE) VALUES('Bob', 'Lee', 20, 'Dallas', 'B');"
+    "INSERT INTO GRADES (NAME, LNAME, AGE, ADDRESS, GRADE) VALUES('Fred', 'Cooper', 24, 'New York', 'C');");
+
+    exit = sqlite3_exec(DB, sql.c_str(), nullptr, 0, &messageError);
+    if(exit != SQLITE_OK){
+        cerr<<"error insert"<<endl;
+        sqlite3_free(messageError);
+    }else{
+        cout<<"Records created sucessfully"<<endl;
+    }
+    return 0;
+}
+
 int main(){
     cout<<"Hello world!"<<endl;
     return 0;
