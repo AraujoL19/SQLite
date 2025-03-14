10f48aeb37812311d59401755344ad34f3bdfdc9
Lucas Araujo <lucasaraujo.oliveira@estudante.ufjf.br>
2025-03-14 10:32:27 -0300

    creates the insertData functions with parameters

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdio.h>
 #include "sqlite3.h"
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -11,6 +13,78 @@ static int createDB(const char* s){
     sqlite3_close(db);
 
     return 0;
+}
+
+static int insertInputData(const char* s, int inputId, const string& cortisolExpParams, char simulationGender, const string& initialConditions){
+    sqlite3* DB;
+    char* messageError;
+    int exit = sqlite3_open(s, &DB);
+
+    string sql = "INSERT INTO INPUT (INPUT_ID, CORTISOL_EXP_PARAMETERS, SIMULATION_GENDER, INITIAL_CONDITIONS) VALUES("
+        + to_string(inputId) + ", '" + cortisolExpParams + "', '" + simulationGender + "', '" + initialConditions + "');"; 
+    exit = sqlite3_exec(DB, sql.c_str(), nullptr, 0, &messageError);
+    if(exit != SQLITE_OK){
+        cerr<<"Error inserting into INPUT: "<<messageError<<endl;
+        sqlite3_free(messageError);
+    }else{
+        cout<<"Record inserted into INPUT sucessfully"<<endl;
+    }
+    sqlite3_close(DB);
+    return exit;
+}
+
+static int insertModelData(const char* s, int modelId, const string& modelDescription, int modelVersion){
+    sqlite3* DB;
+    char* messageError;
+    int exit = sqlite3_open(s, &DB);
+
+    string sql = "INSERT INTO INPUT (MODEL_ID, MODEL_DESCRIPTION, MODEL_VERSION) VALUES("
+        + to_string(modelId) + ", '" + modelDescription + "', " + to_string(modelVersion) + ");"; 
+    exit = sqlite3_exec(DB, sql.c_str(), nullptr, 0, &messageError);
+    if(exit != SQLITE_OK){
+        cerr<<"Error inserting into MODEL: "<<messageError<<endl;
+        sqlite3_free(messageError);
+    }else{
+        cout<<"Record inserted into MODEL sucessfully"<<endl;
+    }
+    sqlite3_close(DB);
+    return exit;
+}
+
+static int insertOutputData(const char* s, int outputId, int modelId, int inputId, const string& results){
+    sqlite3* DB;
+    char* messageError;
+    int exit = sqlite3_open(s, &DB);
+
+    string sql = "INSERT INTO OUTPUT (OUTPUT_ID, MODEL_ID, INPUT_ID, RESULTS) VALUES("
+        + to_string(outputId) + ", " + to_string(modelId) + ", " + to_string(inputId) + ", '" + results + "');"; 
+    exit = sqlite3_exec(DB, sql.c_str(), nullptr, 0, &messageError);
+    if(exit != SQLITE_OK){
+        cerr<<"Error inserting into OUTPUT: "<<messageError<<endl;
+        sqlite3_free(messageError);
+    }else{
+        cout<<"Record inserted into OUTPUT sucessfully"<<endl;
+    }
+    sqlite3_close(DB);
+    return exit;
+}
+
+static int insertSimulationData(const char* s, int simulationId, int outputId, int timeDays, const string& simulationStatus){
+    sqlite3* DB;
+    char* messageError;
+    int exit = sqlite3_open(s, &DB);
+
+    string sql = "INSERT INTO SIMULATION (SIMULATION_ID, OUTPUT_ID, TIME_DAYS, SIMULATION_STATUS) VALUES("
+        + to_string(simulationId) + ", " + to_string(outputId) + ", " + to_string(timeDays) + ", '" + simulationStatus + "');"; 
+    exit = sqlite3_exec(DB, sql.c_str(), nullptr, 0, &messageError);
+    if(exit != SQLITE_OK){
+        cerr<<"Error inserting into SIMULATION: "<<messageError<<endl;
+        sqlite3_free(messageError);
+    }else{
+        cout<<"Record inserted into SIMULATION sucessfully"<<endl;
+    }
+    sqlite3_close(DB);
+    return exit;
 }
 
 static int createSimulationTable(const char* s){
@@ -123,7 +197,7 @@ static int createInputTable(const char* s){
     return 0;
 }
 
-static int insertData(const char* s){
+static int insertDataGlobal(const char* s){
     sqlite3* DB;
     char* messageError;
 
@@ -149,12 +223,26 @@ int main(){
     //const char* dir = "C:\Users\yldog\OneDrive\Documentos\ICimunoendocrino";
     sqlite3* DB;
 
+    //cria a DB no diretório especificado
     createDB(dir);
+    
+    //cria cada uma das tabelas
     createInputTable(dir);
     createModelTable(dir);
     createOutputTable(dir);
     createSimulationTable(dir);
-    insertData(dir);
-    //sqlite3_api was not declared in this scope, 12 29 56 82 109 130 sqlite3_open()
+    
+    //insertDataGlobal(dir); antiga função de inserção de dados
+
+    //insere os dados passados como parâmetro
+    insertInputData(dir, 1, "ParametrosCortisol", 'M', "InitialConditions");
+    insertModelData(dir, 1, "ModelDescription", 1);
+    insertOutputData(dir, 1, 1, 1, "Results");
+    insertSimulationData(dir, 1, 1, 365, "simulation run");
+    
+
+    //erro que ta dando : sqlite3_api was not declared in this scope, 12 29 56 82 109 130 sqlite3_open()
+    //tem que arrumar também os erros de undefined reference
+    
     return 0;
 }
